aaceb0911d568103c4a2ca746f18f02a6f54769e
JEUDominic <xiaoqinaide@live.cn>
2016-04-16 22:16:33 +0800

    Add new problem Same Tree

--- HelloMyLeetcode.c
+++ HelloMyLeetcode.c
@@ -124,3 +124,21 @@ void deleteNode(struct ListNode* node) {
 
 
 
+
+// Same Tree...
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    if(p==NULL&&q==NULL) return true;
+    if(p==NULL&&q!=NULL||q==NULL&&p!=NULL||p->val!=q->val) return false;
+    return isSameTree(p->left,q->left)&&isSameTree(p->right,q->right);
+
+    
+}
+
