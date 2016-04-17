4166911fa749eb6308b9bbce29170ba9074bd15e
JEUDominic <xiaoqinaide@live.cn>
2016-04-17 22:38:41 +0800

    Valid Anagram add new C++ solution

--- HelloMyLeetcode.c
+++ HelloMyLeetcode.c
@@ -157,3 +157,18 @@ public class Solution {
         return String.valueOf(sArr).equals(String.valueOf(tArr));
     }
 }
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int bit[26] = {0}, len = s.length();
+    
+        for(int i=0; i<len; i++)
+            bit[s[i]-'a']++;
+    
+        for(int i=0; i<len; i++)
+            if(--bit[t[i]-'a'] < 0)
+                return false;
+        return true;
+    }
+}; 
