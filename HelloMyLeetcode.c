819adb980080b46fda65acc8d2b1bc9cbfad2b37
username <xiaoqinaide@live.cn>
2016-04-27 23:34:20 +0800

    Add new problem Lowest Common Ancestor

--- HelloMyLeetcode.c
+++ HelloMyLeetcode.c
@@ -437,3 +437,25 @@ public:
 
 
 
+
+// 235 Lowest Common Ancestor of a Binary Search Tree 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+**/
+
+struct TreeNode* lowestCommonAncestor(struct TreeNode* root, struct TreeNode* p, struct TreeNode* q) {
+    
+  if (!root || !p || !q) return NULL;
+  if (p->val < root->val && q->val <root ->val)
+    return lowestCommonAncestor(root->left, p, q);
+  else if (p->val > root->val && q->val > root->val)
+    return lowestCommonAncestor(root->right, p, q);
+  else
+    return root;
+
+}
