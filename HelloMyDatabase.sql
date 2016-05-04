3c0bdd3f5d3cf3591b33ad4ea5339b857b37019c
username <xiaoqinaide@live.cn>
2016-05-05 00:48:23 +0800

    Add Database Problem Duplicate Emails

--- HelloMyDatabase.sql
+++ HelloMyDatabase.sql
@@ -1,4 +1,12 @@
-＃ 181 Employees Earning More Than Their Managers
+# 181 Employees Earning More Than Their Managers
+ 
 SELECT a.Name as Employee
-FROM ( Employee as a inner join Employee as b on a.ManagerID = b.Id) 
+FROM (Employee as a inner join Employee as b 
+		on a.ManagerID = b.Id) 
 WHERE a.Salary > b.Salary
+# 182 Duplicate Emails
+SELECT b.Email
+FROM (SELECT Email, count(Email)  
+		FROM Person as a 
+		GROUP BY Email 
+		HAVING count(Email) >=2 ) as b
