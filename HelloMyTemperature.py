eb826cf73995768d64594fd8f63feea85a585c62
username <xiaoqinaide@live.cn>
2016-05-04 21:54:57 +0800

    Add loop for HelloMyTemperature

--- HelloMyTemperature.py
+++ HelloMyTemperature.py
@@ -1,10 +1,11 @@
-#TemperatureConvert.py
-val = input("Please input temprature with sign (like 32C): ")
-if val[-1] in ['c','C']:
-    f = 1.8 * float(val[0:-1]) + 32
-    print("The temprature after converting is: %.2fF"%f)
-elif val[-1] in ['f','F']:
-    c = (float(val[0:-1]) - 32) / 1.8
-    print("The temprature after converting is: %.2fC"%c)
-else:
-    print("Input invalid.")
+# Code by JEU
+for i in range(3):
+    val = input("Please input temprature with sign (like 32C): ")
+    if val[-1] in ['c','C']:
+        f = 1.8 * float(val[0:-1]) + 32
+        print("The temprature after converting is: %.2fF"%f)
+    elif val[-1] in ['f','F']:
+        c = (float(val[0:-1]) - 32) / 1.8
+        print("The temprature after converting is: %.2fC"%c)
+    else:
+        print("Input invalid.")
